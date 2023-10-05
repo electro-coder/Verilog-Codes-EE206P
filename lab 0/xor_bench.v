4f6c84b8502f137380be343e02f9ce2b83ebd1a2
Sayan Kundu <143341646+electro-coder@users.noreply.github.com>
2023-10-05 09:26:56 +0530

    Create xor_bench.v

---
+++ lab 0/xor_bench.v
@@ -0,0 +1,12 @@
+module xor_bench();
+reg a1,b1;
+wire y1;
+xor_gate dut (.a(a1),.b(b1),.y(y1));
+initial
+begin
+	      a1=1'b0;b1=1'b0;
+#100    a1=1'b0;b1=1'b1;
+#100    a1=1'b1;b1=1'b0;
+#100    a1=1'b1;b1=1'b1;
+end
+endmodule
