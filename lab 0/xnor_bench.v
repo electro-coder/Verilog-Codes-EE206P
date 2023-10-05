d32feb2a563290cea23ab840b2d9147697843073
Sayan Kundu <143341646+electro-coder@users.noreply.github.com>
2023-10-05 09:27:56 +0530

    Create xnor_bench.v

---
+++ lab 0/xnor_bench.v
@@ -0,0 +1,12 @@
+module xnor_bench();
+reg a1,b1;
+wire y1;
+xnor_gate dut (.a(a1),.b(b1),.y(y1));
+initial
+begin
+	      a1=1'b0;b1=1'b0;
+#100    a1=1'b0;b1=1'b1;
+#100    a1=1'b1;b1=1'b0;
+#100    a1=1'b1;b1=1'b1;
+end
+endmodule
