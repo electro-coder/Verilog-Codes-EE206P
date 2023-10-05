8712ed54562ce4ae43496f552b5021b868e40b79
Sayan Kundu <143341646+electro-coder@users.noreply.github.com>
2023-10-05 09:22:07 +0530

    Create or_bench.v

---
+++ lab 0/or_bench.v
@@ -0,0 +1,12 @@
+module or_bench();
+reg a1,b1;
+wire y1;
+or_gate dut (.a(a1),.b(b1),.y(y1));
+initial
+begin
+	      a1=1'b0;b1=1'b0;
+#100    a1=1'b0;b1=1'b1;
+#100    a1=1'b1;b1=1'b0;
+#100    a1=1'b1;b1=1'b1;
+end
+endmodule
